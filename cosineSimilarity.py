ffb11885c340fefd8dea11aaa0686dcc65832a11
Rushil Gupta <rgupta@tunein.com>
2015-06-07 03:29:59 -0700

    output data in decreasing similarity value, use path as cmdline arg

--- cosineSimilarity.py
+++ cosineSimilarity.py
@@ -1,4 +1,5 @@
 import math
+import argparse
 
 def formVector(filePath):
     vektors = {}
@@ -25,11 +26,31 @@ def magnitude(vektor):
         magnitude += vektor[key]*vektor[key]
     return math.sqrt(magnitude)
 
-vektors=formVector('/Users/rgupta/Downloads/stock-similarity/stock2.data')
+def sortedSimilarity(item, matrix):
+    print 'printing similarity order for %s ' %(item)
+    item_map = {}
+    for i in xrange(len(matrix)):
+        item_map[matrix[item][i]] = i
+
+    keylist = item_map.keys()
+    keylist.sort(reverse=True)
+    for key in keylist:
+        #print "%s: %s" % (key, item_map[key])
+        print("%s," % (item_map[key])),
+    print ''
+
+parser = argparse.ArgumentParser()
+# usage: python cosineSimilarity.py --path  /Users/rgupta/Downloads/stock-similarity/stock.data
+parser.add_argument('--path', help='path to file in format item:user:rating')
+args = parser.parse_args()
+
+vektors=formVector(args.path)
 matrix = [[0 for i in xrange(len(vektors))] for i in xrange(len(vektors))]
 
 for item1 in vektors:
     for item2 in vektors:
         if item1 != item2:
             matrix[item1][item2] = cosineSimilarity(item1, item2, vektors)
-print matrix
+
+for item in vektors:
+    sortedSimilarity(item, matrix)
--- suckData.py
+++
@@ -1,9 +0,0 @@
-import twitter
-
-api = twitter.Api(consumer_key='yVJ1IbE5zN2qoXyurztCatY5v', consumer_secret='DdnOwQCcmsD1KBWXsEWi8bmhk2FsBhoXtZTF22iYbDzfjuzkmb',
-  access_token_key='113862838-urs2yuzG8qdqZ7pJqpxqptN0iVI0bEKXjsXXW37r',access_token_secret='iX17wJcs4fkjpjRQd9duYdMQLIPxyESpAOBVl6a08XSbo')
-
-#GetSearch(self, term=None, geocode=None, since_id=None, max_id=None, until=None, since=None, count=15, lang=None, locale=None, result_type="mixed", include_entities=None):
-dataset = api.GetSearch("aapl", count=10)
-for data in dataset:
-    print data.text
